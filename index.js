62ef9af9a780e8a8114fa7f9034826a8c5eab470
Yevhenii Zhdan <yevhenii.zhdan@preply.com>
2024-02-21 11:53:59 +0200

    class creation

---
+++ index.js
@@ -0,0 +1,26 @@
+class ECTS {
+    constructor(score) {
+        this.score = score;
+    }
+
+    ectsFromScore() {
+        switch (this.score) {
+            case this.score >= 90:
+                return 'A';
+            case this.score >= 82:
+                return 'B';
+            case this.score >= 74:
+                return 'C';
+            case this.score >= 65:
+                return 'D';
+            case this.score >= 60:
+                return 'E';
+            case this.score < 60:
+                return 'F';
+            default:
+                return 'Invalid score';
+        }
+    }
+}
+
+export default ECTS;
