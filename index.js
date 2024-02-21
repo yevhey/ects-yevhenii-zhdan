c9365f600c6f6f661f0543ca9eed61ab43679170
Yevhenii Zhdan <yevhenii.zhdan@preply.com>
2024-02-21 12:10:11 +0200

    d

--- index.js
+++ index.js
@@ -4,22 +4,23 @@ class ECTS {
     }
 
     ectsFromScore() {
-        switch (this.score) {
-            case this.score >= 90:
-                return 'A';
-            case this.score >= 82:
-                return 'B';
-            case this.score >= 74:
-                return 'C';
-            case this.score >= 65:
-                return 'D';
-            case this.score >= 60:
-                return 'E';
-            case this.score < 60:
-                return 'F';
-            default:
-                return 'Invalid score';
+        if (this.score >= 90) {
+            return 'A';
         }
+        if (this.score >= 82) {
+            return 'B';
+        }
+        if (this.score >= 74) {
+            return 'C';
+        }
+        if (this.score >= 65) {
+            return 'D';
+        }
+        if (this.score >= 60) {
+            return 'E';
+        }
+
+        return 'F';
     }
 }
 
